97714087e0014a43bb00345d6928e43dc1940563
Andrea N <andreain3d@gmail.com>
2022-03-31 19:35:36 -0400

    added placeholder index.html, set up endpoint for user info

--- src/main/java/com/example/cardtoggler/cardtoggler/controllers/UserInfoController.java
+++ src/main/java/com/example/cardtoggler/cardtoggler/controllers/UserInfoController.java
@@ -1,4 +1,35 @@
 package com.example.cardtoggler.cardtoggler.controllers;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class UserInfoController {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    public String userInfo;
+
+    @GetMapping(value = "/userInfo")
+
+    public String userInfo() {
+        String url = "https://anypoint.mulesoft.com/mocking/api/v1/links/2107a7ca-f0f9-4894-93f3-a6f18e9c9f63/cardInfo/12345";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("API-Key", apiKey);
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("Request Successful.");
+            System.out.println(response.getBody());
+        } else {
+            System.out.println(response.getStatusCode());
+        }
+        return response.getBody();
+    }
+
 }
