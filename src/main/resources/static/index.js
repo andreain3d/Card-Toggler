3d627719d0a4c1ad6ae4a5f613b133e9990fcecd
Andrea N <andreain3d@gmail.com>
2022-04-01 18:51:44 -0400

    added ajax call to get user info from server

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,10 +1,35 @@
 const userInfo = {
-    "cardHolder":"Dave Normalname",
-    "cards":[
-        {"cardId":"0001","cardName":"Rewards Card","maskedCardNumber":"xx0455", "active":true},
-        {"cardId":"0002","cardName":"Cash Back Card","maskedCardNumber":"xx0456", "active":true}
-    ]
+//    "cardHolder":"Dave Normalname",
+//    "cards":[
+//        {"cardId":"0001","cardName":"Rewards Card","maskedCardNumber":"xx0455", "active":true},
+//        {"cardId":"0002","cardName":"Cash Back Card","maskedCardNumber":"xx0456", "active":true}
+//    ]
 };
+
+function getUserInfo () {
+    $.ajax({
+        url: '/userInfo',
+        type: "GET",
+        success: function (data) {
+            let info = JSON.parse(data);
+            userInfo.cardHolder = info.cardHolder;
+            userInfo.cards = info.cards;
+            //initializing 'active' status as true as dummy info for the property
+            for (let i = 0; i < userInfo.cards.length; i++){
+                userInfo.cards[i].active = true;
+            }
+            console.log("Retrieved userInfo: " + JSON.stringify(userInfo));
+            for (let i=0; i < userInfo.cards.length; i++ ){
+                $('#card-select').append("<option value=\"" + userInfo.cards[i].maskedCardNumber + "\">Card ending in " + userInfo.cards[i].maskedCardNumber + "</option>")
+            }
+            $('#greeting').text('Welcome, ' + userInfo.cardHolder + '!')
+            setCardImage();
+        },
+        error: function (error) {
+            console.log(`Error retrieving user data: ${error}`);
+        }
+    })
+}
 
 function cardChangeHandler () {
     $('#card-select').change(
@@ -46,14 +71,7 @@ function setCardImage () {
 }
 
 $(document).ready(function(){
+    getUserInfo();
     cardChangeHandler();
     toggleActivationHandler();
-
-    $('#greeting').text('Welcome, ' + userInfo.cardHolder + '!')
-
-    for (let i=0; i < userInfo.cards.length; i++ ){
-        $('#card-select').append("<option value=\"" + userInfo.cards[i].maskedCardNumber + "\">Card ending in " + userInfo.cards[i].maskedCardNumber + "</option>")
-    }
-
-    setCardImage();
 });
