6ca612b073f68e41a605fffc1f3a7d4206fd2065
Andrea N <andreain3d@gmail.com>
2022-03-31 20:51:46 -0400

    set up endpoint for onoff

---
+++ src/main/java/com/example/cardtoggler/cardtoggler/controllers/OnOffController.java
@@ -0,0 +1,34 @@
+package com.example.cardtoggler.cardtoggler.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OnOffController {
+
+    @Value("${api.key}")
+    private String apiKey;
+    
+    public String onOff;
+
+    @PostMapping("/onOff")
+    public String onOff() {
+        String url = "https://anypoint.mulesoft.com/mocking/api/v1/links/2107a7ca-f0f9-4894-93f3-a6f18e9c9f63/cardcontrols/onoff/0001";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("API-Key", apiKey);
+        String body = "";
+        HttpEntity<String> request = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            System.out.println("Request Successful.");
+            System.out.println(response.getBody());
+        } else {
+            System.out.println(response.getStatusCode());
+        }
+        return response.getBody();
+    }
+}
